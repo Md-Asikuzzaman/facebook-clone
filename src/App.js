92269b1c2fb44ee2b7d819a250f18864310b852b
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2021-12-25 19:21:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
-import "./style/style.css";
-import Sidebar from "./Sidebar";
-import Chat from "./Chat";
-import "./style/app.css";
-import "./style/icon/css/font-awesome.min.css";
+import Main from "./components/Main";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <div className="app__body">
-          <Sidebar />
-          <Chat />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
 
 export default App;
